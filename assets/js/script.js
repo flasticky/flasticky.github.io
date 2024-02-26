01f26deb9432779f50b02442254dcd568714a516
flasticky <jan.flaska@gmail.com>
2024-02-26 13:20:42 +0100

    feat: Portfolio section creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,3 +97,18 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+/* === Portfolio swiper === */
+let swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: 'swiper-button-next',
+        prevEl: 'swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
